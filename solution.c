cd289d66bbdbd2c097f07af11b12e571a0ee9143
H3X490N157 <73405139+H3X490N157@users.noreply.github.com>
2025-02-23 12:22:45 +0300

    Add files via upload

---
+++ solution.c
@@ -0,0 +1,234 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct FactorNode {
+    char factor;
+    struct FactorNode *next;
+} FactorNode;
+
+typedef struct TermNode {
+    int coefficient;
+    FactorNode *factors;
+    struct TermNode *next;
+} TermNode;
+
+FactorNode* create_factor(char factor) {
+    FactorNode *new_factor = (FactorNode *)malloc(sizeof(FactorNode));
+    new_factor->factor = factor;
+    new_factor->next = NULL;
+    return new_factor;
+}
+
+void add_factor_sorted(FactorNode **head, char factor) {
+    FactorNode *new_factor = create_factor(factor);
+    if (!*head || (*head)->factor > factor) {
+        new_factor->next = *head;
+        *head = new_factor;
+        return;
+    }
+    FactorNode *current = *head;
+    while (current->next && current->next->factor < factor) {
+        current = current->next;
+    }
+    new_factor->next = current->next;
+    current->next = new_factor;
+}
+
+TermNode* create_term(int coefficient) {
+    TermNode *new_term = (TermNode *)malloc(sizeof(TermNode));
+    new_term->coefficient = coefficient;
+    new_term->factors = NULL;
+    new_term->next = NULL;
+    return new_term;
+}
+
+void add_term(TermNode **head, int coefficient, FactorNode *factors) {
+    TermNode *new_term = create_term(coefficient);
+    new_term->factors = factors;
+
+    if (!*head) {
+        *head = new_term;
+        return;
+    }
+    TermNode *current = *head;
+    while (current->next) {
+        current = current->next;
+    }
+    current->next = new_term;
+}
+
+int compare_factors(FactorNode *f1, FactorNode *f2) {
+    while (f1 && f2) {
+        if (f1->factor != f2->factor) return f1->factor - f2->factor;
+        f1 = f1->next;
+        f2 = f2->next;
+    }
+    return (f1 ? 1 : 0) - (f2 ? 1 : 0);
+}
+
+void sort_terms(TermNode **head) {
+    if (!*head || !(*head)->next) return;
+    TermNode *sorted = NULL;
+    TermNode *current = *head;
+
+    while (current) {
+        TermNode *next = current->next;
+        if (!sorted || compare_factors(current->factors, sorted->factors) < 0) {
+            current->next = sorted;
+            sorted = current;
+        } else {
+            TermNode *temp = sorted;
+            while (temp->next && compare_factors(temp->next->factors, current->factors) < 0) {
+                temp = temp->next;
+            }
+            current->next = temp->next;
+            temp->next = current;
+        }
+        current = next;
+    }
+    *head = sorted;
+}
+
+void simplify(TermNode **head) {
+    if (!*head) return;
+    sort_terms(head);
+
+    TermNode *current = *head, *prev = NULL;
+    while (current) {
+        TermNode *runner = current->next, *runner_prev = current;
+        while (runner) {
+            if (compare_factors(current->factors, runner->factors) == 0) {
+                current->coefficient += runner->coefficient;
+                runner_prev->next = runner->next;
+                free(runner);
+                runner = runner_prev->next;
+            } else {
+                runner_prev = runner;
+                runner = runner->next;
+            }
+        }
+        if (current->coefficient == 0) {
+            if (prev) prev->next = current->next;
+            else *head = current->next;
+            free(current);
+            current = (prev ? prev->next : *head);
+        } else {
+            prev = current;
+            current = current->next;
+        }
+    }
+}
+
+void parse_psform(char *input, TermNode **head) {
+    int sign = 1, coefficient;
+    char *ptr = input;
+    while (*ptr) {
+        while (*ptr == ' ') ptr++;
+        if (*ptr == '+' || *ptr == '-') {
+            sign = (*ptr == '-') ? -1 : 1;
+            ptr++;
+        } else sign = 1;
+
+        while (*ptr == ' ') ptr++;
+
+        coefficient = sign;
+        if (isdigit(*ptr)) {
+            coefficient = sign * atoi(ptr);
+            while (isdigit(*ptr)) ptr++;
+        }
+
+        FactorNode *factor_list = NULL;
+        while (*ptr && *ptr != '+' && *ptr != '-') {
+            if (isalpha(*ptr)) {
+                add_factor_sorted(&factor_list, *ptr);
+                ptr++;
+            } else ptr++;
+        }
+
+        add_term(head, coefficient, factor_list);
+    }
+}
+
+void print_psform(TermNode *head) {
+    if (!head) {
+        printf("0\n");
+        return;
+    }
+
+    TermNode *current = head;
+    while (current) {
+        if (current != head) {
+            printf(current->coefficient < 0 ? " - " : " + ");
+        } else if (current->coefficient < 0) {
+            printf("-");
+        }
+
+        int abs_coeff = abs(current->coefficient);
+        if (abs_coeff != 1 || !current->factors)
+            printf("%d", abs_coeff);
+
+        FactorNode *factor = current->factors;
+        while (factor) {
+            printf("%c", factor->factor);
+            factor = factor->next;
+        }
+
+        current = current->next;
+    }
+    printf("\n");
+}
+
+int are_equal(TermNode *head1, TermNode *head2) {
+    while (head1 && head2) {
+        if (head1->coefficient != head2->coefficient ||
+            compare_factors(head1->factors, head2->factors) != 0)
+            return 0;
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    return head1 == head2;
+}
+
+void free_psform(TermNode *head) {
+    while (head) {
+        FactorNode *factor = head->factors;
+        while (factor) {
+            FactorNode *tmp_factor = factor;
+            factor = factor->next;
+            free(tmp_factor);
+        }
+        TermNode *tmp_term = head;
+        head = head->next;
+        free(tmp_term);
+    }
+}
+
+int main() {
+    char operation;
+    char input1[1000], input2[1000];
+
+    scanf(" %c", &operation);
+    scanf(" %[^\n]", input1);
+    scanf(" %[^\n]", input2);
+
+    TermNode *ps1 = NULL, *ps2 = NULL;
+    parse_psform(input1, &ps1);
+    parse_psform(input2, &ps2);
+
+    simplify(&ps1);
+    simplify(&ps2);
+
+    print_psform(ps1);
+    print_psform(ps2);
+
+    if (operation == '=') {
+        printf(are_equal(ps1, ps2) ? "equal\n" : "not equal\n");
+    }
+
+    free_psform(ps1);
+    free_psform(ps2);
+
+    return 0;
+}
